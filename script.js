d17067a08d247eba783f7ad4a8e67904266e4e5b
bandesz50 <76780228+bandesz50@users.noreply.github.com>
2021-01-05 22:23:19 +0100

    hu öcsém
    hát ezt nem ebbő
    fogom megtanulni

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
  console.log("Timkó András")
- 
+ let able = !false;
+if (able === true) {
+  console.log('The condition has been evaluated to true');
+}
+let a = 4;
+if (a < 5) {
+  console.log(a / 2);
+} else {
+  console.log(a * 2);
+}
+for (let i = 0; i < 10; i++) {console.log(i);} 
+
+let bookCount = 11;
+
+for (let m = 0; m < bookCount; m++) {console.log(m);}
+
+for (let p = 1; p < 11; p++) {console.log(p);}   
+
+  
+  console.log(add(1, 2)) // will print 3
